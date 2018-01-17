90124b16ca2ab309d678e322beaa841a5536b4be
smohammad <shabbir.mohammad@noblis.org>
2018-01-17 16:38:47 -0500

    ppppppppppp

---
+++ JenkinTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinTest.Startup))]
+namespace JenkinTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
